66c3a027f155cddcf00d6481d9f6b05257da3318
gravitano <gue.warsono@gmail.com>
2014-09-10 23:11:31 +0700

    Fix service provider error

--- src/Pingpong/Oembed/OembedServiceProvider.php
+++ src/Pingpong/Oembed/OembedServiceProvider.php
@@ -1,6 +1,5 @@
 <?php namespace Pingpong\Oembed;
 
-use Embed\Embed;
 use Illuminate\Support\ServiceProvider;
 
 class OembedServiceProvider extends ServiceProvider {
@@ -31,7 +30,7 @@ class OembedServiceProvider extends ServiceProvider {
 	{
 		$this->app->bindShared('oembed', function ($app)
 		{
-			return new Oembed(new Embed, $app['cache']);
+			return new Oembed(new Embed, $app['cache.store']);
 		});
 	}
 
