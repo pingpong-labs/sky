c4e070eea011978e2ae52449a61763b31718cd06
gravitano <gravitano16@gmail.com>
2015-02-04 17:34:29 +0700

    Update file structure

--- tests/Oembed/OembedTest.php
+++ tests/Oembed/OembedTest.php
@@ -6,43 +6,45 @@ use Pingpong\Oembed\Oembed;
 class OembedTest extends PHPUnit_Framework_TestCase {
 
     protected $embed;
+
     protected $cache;
+
     protected $oembed;
 
-	public function setUp()
-	{
-		$this->embed = m::mock('Pingpong\Oembed\Embed');
+    public function setUp()
+    {
+        $this->embed = m::mock('Pingpong\Oembed\Embed');
         $this->cache = m::mock('Illuminate\Cache\Repository');
-		$this->oembed = new Oembed($this->embed, $this->cache);
-	}
+        $this->oembed = new Oembed($this->embed, $this->cache);
+    }
 
-	public function test_initialize()
-	{
-		$this->assertInstanceOf('Pingpong\Oembed\Oembed', $this->oembed);
-	}
+    public function test_initialize()
+    {
+        $this->assertInstanceOf('Pingpong\Oembed\Oembed', $this->oembed);
+    }
 
-	public function test_get_info_from_a_specify_url()
-	{
-		$url = 'https://github.com/gravitano';
+    public function test_get_info_from_a_specify_url()
+    {
+        $url = 'https://github.com/gravitano';
 
         $this->embed->shouldReceive('get')->once()->with($url, '')->andReturn(array('url' => $url));
 
         $info = $this->oembed->get($url);
 
-		$this->assertTrue(is_array($info));
-		$this->assertArrayHasKey('url', $info);
-	}
+        $this->assertTrue(is_array($info));
+        $this->assertArrayHasKey('url', $info);
+    }
 
-	public function test_get_info_from_a_specify_url_but_false_returned()
-	{
-		$url = 'foo';
+    public function test_get_info_from_a_specify_url_but_false_returned()
+    {
+        $url = 'foo';
 
         $this->embed->shouldReceive('get')->once()->with($url, '')->andReturn(false);
 
         $info = $this->oembed->get($url);
 
-		$this->assertFalse($info);
-	}
+        $this->assertFalse($info);
+    }
 
     public function test_get_info_from_a_specify_url_and_also_use_laravel_cache_manager()
     {
