60e54b38d4fea7637633cab9f781a31a28af1364
gravitano <gravitano16@gmail.com>
2015-02-05 12:00:38 +0700

    Update branch

--- build/push.sh
+++ build/push.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-git push origin master
-git subtree push --prefix=src/Pingpong/Widget git@github.com:pingpong-labs/widget.git master
-git subtree push --prefix=src/Pingpong/Shortcode git@github.com:pingpong-labs/shortcode.git master
-git subtree push --prefix=src/Pingpong/Menus git@github.com:pingpong-labs/menus.git master
-git subtree push --prefix=src/Pingpong/Presenters git@github.com:pingpong-labs/presenters.git master
-git subtree push --prefix=src/Pingpong/Modules git@github.com:pingpong-labs/modules.git master
-git subtree push --prefix=src/Pingpong/Testing git@github.com:pingpong-labs/testing.git master
-git subtree push --prefix=src/Pingpong/Generators git@github.com:pingpong-labs/generators.git master
-git subtree push --prefix=src/Pingpong/Validator git@github.com:pingpong-labs/validator.git master
-git subtree push --prefix=src/Pingpong/Oembed git@github.com:pingpong-labs/oembed.git master
-git subtree push --prefix=src/Pingpong/Trusty git@github.com:pingpong-labs/trusty.git master
-git subtree push --prefix=src/Pingpong/Themes git@github.com:pingpong-labs/themes.git master
+git push origin 2.0
+git subtree push --prefix=src/Pingpong/Widget git@github.com:pingpong-labs/widget.git 2.0
+git subtree push --prefix=src/Pingpong/Shortcode git@github.com:pingpong-labs/shortcode.git 2.0
+git subtree push --prefix=src/Pingpong/Menus git@github.com:pingpong-labs/menus.git 2.0
+git subtree push --prefix=src/Pingpong/Presenters git@github.com:pingpong-labs/presenters.git 2.0
+git subtree push --prefix=src/Pingpong/Modules git@github.com:pingpong-labs/modules.git 2.0
+git subtree push --prefix=src/Pingpong/Testing git@github.com:pingpong-labs/testing.git 2.0
+git subtree push --prefix=src/Pingpong/Generators git@github.com:pingpong-labs/generators.git 2.0
+git subtree push --prefix=src/Pingpong/Validator git@github.com:pingpong-labs/validator.git 2.0
+git subtree push --prefix=src/Pingpong/Oembed git@github.com:pingpong-labs/oembed.git 2.0
+git subtree push --prefix=src/Pingpong/Trusty git@github.com:pingpong-labs/trusty.git 2.0
+git subtree push --prefix=src/Pingpong/Themes git@github.com:pingpong-labs/themes.git 2.0
