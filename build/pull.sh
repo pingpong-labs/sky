f75689114e136965a887288c161fc27d0e32583e
gravitano <gravitano16@gmail.com>
2015-02-05 10:52:57 +0700

    Add pull script

---
+++ build/pull.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+git pull origin master
+git subtree pull --prefix=src/Pingpong/Widget git@github.com:pingpong-labs/widget.git master
+git subtree pull --prefix=src/Pingpong/Shortcode git@github.com:pingpong-labs/shortcode.git master
+git subtree pull --prefix=src/Pingpong/Menus git@github.com:pingpong-labs/menus.git master
+git subtree pull --prefix=src/Pingpong/Presenters git@github.com:pingpong-labs/presenters.git master
+git subtree pull --prefix=src/Pingpong/Modules git@github.com:pingpong-labs/modules.git master
+git subtree pull --prefix=src/Pingpong/Testing git@github.com:pingpong-labs/testing.git master
+git subtree pull --prefix=src/Pingpong/Generators git@github.com:pingpong-labs/generators.git master
+git subtree pull --prefix=src/Pingpong/Validator git@github.com:pingpong-labs/validator.git master
+git subtree pull --prefix=src/Pingpong/Oembed git@github.com:pingpong-labs/oembed.git master
