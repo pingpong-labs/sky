888bf074d1e9082ffe51b6ff30a47f28cec19207
gravitano <gravitano16@gmail.com>
2015-02-16 18:04:57 +0700

    Squashed 'src/Pingpong/Shortcode/' changes from c05c9fd..efb7b61
    efb7b61 Remove boot method
    89b4e47 documentation moved to official website
    7c7c5ba Fix namespace
    f2c742f Update to L5
    e9fba9e Upgrade to PSR-4
    b723a9d Upgrade some require-dev dependencies
    552d56e Remove support for Laravel 5
    10ce242 Reformat code
    48f754d Move shortcode facade
    a2538b4 Update file structure for pingpong/widget
    
    git-subtree-dir: src/Pingpong/Shortcode
    git-subtree-split: efb7b614aab9d4150d30d71faf9e1bf4f7c15e37

---
+++ ShortcodeFacade.php
@@ -0,0 +1,14 @@
+<?php namespace Pingpong\Shortcode;
+
+use Illuminate\Support\Facades\Facade;
+
+class ShortcodeFacade extends Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'shortcode'; }
+
+}
