c6e9203efd44f5db00f7a10f2b098d7426ad93f4
Gravitano <gravitano16@gmail.com>
2015-04-11 17:20:49 +0700

    Add docblock

--- src/Pingpong/Support/ImageTrait.php
+++ src/Pingpong/Support/ImageTrait.php
@@ -2,6 +2,11 @@
 
 trait ImageTrait {
 
+    /**
+     * Delete image from current instance.
+     * 
+     * @return boolean
+     */
     public function deleteImage()
     {
         if (file_exists($path = $this->getImagePath()))
@@ -14,21 +19,42 @@ trait ImageTrait {
         return false;
     }
 
+    /**
+     * Get image path.
+     * 
+     * @return string
+     */
     public function getImagePath()
     {
         return public_path(static::$path . $this->image);
     }
 
+    /**
+     * Get image url.
+     * 
+     * @return string
+     */
     public function getImageUrl()
     {
         return asset(static::$path . $this->image);
     }
 
+    /**
+     * Accessor for "image_url".
+     * 
+     * @param  string $value
+     * @return string
+     */
     public function getImageUrlAttribute($value)
     {
         return $this->getImageUrl();
     }
 
+    /**
+     * Determine whether the current file has image.
+     * 
+     * @return boolean
+     */
     public function hasImage()
     {
         return ! empty($this->image) && file_exists($this->getImagePath());
