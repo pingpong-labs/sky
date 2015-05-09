4e598eac4dd2832a7ed3deb560dd9a59e01d99da
Gravitano <gravitano16@gmail.com>
2015-05-09 13:07:44 +0700

    change branch

--- build/pull.sh
+++ build/pull.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-git pull origin 2.0
-git subtree pull --prefix=src/Pingpong/Widget git@github.com:pingpong-labs/widget.git 2.0 --squash
-git subtree pull --prefix=src/Pingpong/Shortcode git@github.com:pingpong-labs/shortcode.git 2.0 --squash
-git subtree pull --prefix=src/Pingpong/Menus git@github.com:pingpong-labs/menus.git 2.0 --squash
-git subtree pull --prefix=src/Pingpong/Presenters git@github.com:pingpong-labs/presenters.git 2.0 --squash
-git subtree pull --prefix=src/Pingpong/Modules git@github.com:pingpong-labs/modules.git 2.0 --squash
-git subtree pull --prefix=src/Pingpong/Testing git@github.com:pingpong-labs/testing.git 2.0 --squash
-git subtree pull --prefix=src/Pingpong/Generators git@github.com:pingpong-labs/generators.git 2.0 --squash
-git subtree pull --prefix=src/Pingpong/Trusty git@github.com:pingpong-labs/trusty.git 2.0 --squash
-git subtree pull --prefix=src/Pingpong/Themes git@github.com:pingpong-labs/themes.git 2.0 --squash
-git subtree pull --prefix=src/Pingpong/Support git@github.com:pingpong-labs/support.git 2.0 --squash
+git pull origin master
+git subtree pull --prefix=src/Pingpong/Widget git@github.com:pingpong-labs/widget.git master --squash
+git subtree pull --prefix=src/Pingpong/Shortcode git@github.com:pingpong-labs/shortcode.git master --squash
+git subtree pull --prefix=src/Pingpong/Menus git@github.com:pingpong-labs/menus.git master --squash
+git subtree pull --prefix=src/Pingpong/Presenters git@github.com:pingpong-labs/presenters.git master --squash
+git subtree pull --prefix=src/Pingpong/Modules git@github.com:pingpong-labs/modules.git master --squash
+git subtree pull --prefix=src/Pingpong/Testing git@github.com:pingpong-labs/testing.git master --squash
+git subtree pull --prefix=src/Pingpong/Generators git@github.com:pingpong-labs/generators.git master --squash
+git subtree pull --prefix=src/Pingpong/Trusty git@github.com:pingpong-labs/trusty.git master --squash
+git subtree pull --prefix=src/Pingpong/Themes git@github.com:pingpong-labs/themes.git master --squash
+git subtree pull --prefix=src/Pingpong/Support git@github.com:pingpong-labs/support.git master --squash
