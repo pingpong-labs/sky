6f6b12c0c1f9fddd830c0b362a4f189e2db44f26
Gravitano <gravitano16@gmail.com>
2015-05-09 13:17:32 +0700

    Squashed 'src/Pingpong/Menus/' changes from b86fbd2..ff0c03c
    git-subtree-dir: src/Pingpong/Menus
    git-subtree-split: ff0c03ca0ebf8fb73e757f4cb2741b04851fe584

--- MenusServiceProvider.php
+++ MenusServiceProvider.php
@@ -1,100 +1,61 @@
 <?php namespace Pingpong\Menus;
 
-use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
 class MenusServiceProvider extends ServiceProvider {
 
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
 
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->registerNamespaces();
-        $this->registerMenusFile();
-    }
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('pingpong/menus');
+		$this->requireMenusFile();
+	}
 
-    /**
-     * Require the menus file if that file is exists.
-     *
-     * @return void
-     */
-    public function registerMenusFile()
-    {
-        if (file_exists($file = app_path('Support/menus.php')))
+	/**
+	 * Require the menus file if that file is exists.
+	 *
+	 * @return void
+	 */
+	public function requireMenusFile()
+	{
+		if(file_exists($file = app_path('menus.php')))
+		{
+			require $file;
+		}
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+        $this->app['menus'] = $this->app->share(function($app)
         {
-            require $file;
-        }
-    }
-
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->registerIlluminateHtml();
-
-        $this->app['menus'] = $this->app->share(function ($app)
-        {
-            return new Menu($app['view'], $app['config']);
+        	return new Menu($app['view'], $app['config']);
         });
-    }
+	}
 
-    /**
-     * Register "iluminate/html" package.
-     *
-     * @return void
-     */
-    protected function registerIlluminateHtml()
-    {
-        $this->app->register('Illuminate\Html\HtmlServiceProvider');
-
-        $aliases = [
-            'HTML' => 'Illuminate\Html\HtmlFacade',
-            'Form' => 'Illuminate\Html\FormFacade',
-        ];
-
-        AliasLoader::getInstance($aliases)->register();
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return array('menus');
-    }
-
-    /**
-     * Register package's namespaces.
-     *
-     * @return void
-     */
-    protected function registerNamespaces()
-    {
-        $this->mergeConfigFrom(__DIR__ . '/src/config/config.php', 'menus');
-        $this->loadViewsFrom(__DIR__ . '/src/views', 'menus');
-
-        $this->publishes([
-            __DIR__ . '/src/config/config.php' => config_path('menus.php'),
-        ], 'config');
-
-        $this->publishes([
-            __DIR__ . '/src/views' => base_path('resources/views/vendor/pingpong/menus'),
-        ], 'views');
-    }
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('menus');
+	}
 
 }
