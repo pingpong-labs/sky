ed6c0d994f57c1808a15d853545f466ecc978422
Gravitano <gravitano16@gmail.com>
2015-05-09 11:57:53 +0700

    re-merge master branch

--- tests/Themes/ThemeTest.php
+++ tests/Themes/ThemeTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Mockery as m;
+use Pingpong\Themes\Repository;
 use Pingpong\Themes\Theme;
 
 class ThemeTest extends PHPUnit_Framework_TestCase {
@@ -15,132 +16,171 @@ class ThemeTest extends PHPUnit_Framework_TestCase {
         return __DIR__ . '/../../fixture/themes/';
     }
 
-    function testInitialize()
+    public function setUp()
     {
-        $finder = m::mock('Pingpong\Themes\Finder');
-        $config = m::mock('Illuminate\Config\Repository');
-        $view = m::mock('Illuminate\View\Factory');
-        $lang = m::mock('Illuminate\Translation\Translator');
+        $this->finder = m::mock('Pingpong\Themes\Finder');
+        $this->config = m::mock('Illuminate\Config\Repository');
+        $this->view = m::mock('Illuminate\View\Factory');
+        $this->lang = m::mock('Illuminate\Translation\Translator');
+        $this->cache = m::mock('Illuminate\Cache\Repository');
+        $this->theme = new Repository(
+            $this->finder,
+            $this->config,
+            $this->view,
+            $this->lang,
+            $this->cache
+        );
 
-        $theme = new Theme($finder, $config, $view, $lang);
-
-        $this->assertInstanceOf('Pingpong\Themes\Theme', $theme);
+        $this->theme->setPath($this->getPath());
     }
 
-    function testGetAllThemes()
+    public function getTheme($name = 'foo')
     {
-        $finder = m::mock('Pingpong\Themes\Finder');
-        $config = m::mock('Illuminate\Config\Repository');
-        $view = m::mock('Illuminate\View\Factory');
-        $lang = m::mock('Illuminate\Translation\Translator');
+        return new Theme([
+            'name' => $name
+        ]);
+    }
 
-        $theme = new Theme($finder, $config, $view, $lang);
+    public function testGetAllThemesAndWillReturnFromFinder()
+    { 
+        $this->config->shouldReceive('get')
+            ->once()
+            ->andReturn(false);
 
-        $finder->shouldReceive('all')->once()->andReturn(['default']);
+        $this->finder->shouldReceive('find')
+                     ->with($this->getPath())
+                     ->once()
+                     ->andReturn([
+                        $this->getTheme()
+                    ]);
+                     
 
-        $themes = $theme->all();
+        $themes = $this->theme->all();
+        $this->assertTrue(is_array($themes));
+    }
+
+    public function testGetAllThemesAndWillReturnFromCache()
+    { 
+        $this->config->shouldReceive('get')
+            ->times(2)
+            ->andReturn(true);
+
+        $this->cache->shouldReceive('get')
+            ->once()
+            ->andReturn([
+                [
+                    'name' => 'foo'
+                ],
+            ]);                     
+
+        $themes = $this->theme->all();
+        $this->assertTrue(is_array($themes));
+    }
+
+    public function testSetAndGetThemePath()
+    {
+        $this->theme->setPath('path/to');
+        $this->assertEquals('path/to', $this->theme->getPath());
+    }
+
+    public function testRegisterNamespaces()
+    {        
+        $this->config->shouldReceive('get')
+            ->once()
+            ->andReturn(false);
+
+        $this->finder->shouldReceive('find')
+                     ->with($this->getPath())
+                     ->once()
+                     ->andReturn([
+                        $this->getTheme()
+                    ]);
+        $this->view->shouldReceive('addNamespace')->once();
+        $this->lang->shouldReceive('addNamespace')->once();
+        $themes = $this->theme->registerNamespaces();
+    }
+
+    public function testGetThemePathForSpecifiedTheme()
+    {
+        $this->theme->setPath('path/to');
+        $themePath = $this->theme->getThemePath('default');
+        $this->assertEquals('path/to/default', $themePath);
+    }
+
+    public function testSetAndGetCurrentTheme()
+    {
+        $this->theme->setCurrent('foo');
+        $this->assertEquals('foo', $this->theme->getCurrent());
+    }
+
+    public function testHasTheme()
+    {        
+        $this->config->shouldReceive('get')
+            ->times(3)
+            ->andReturn(false);
+
+        $this->finder->shouldReceive('find')
+                     ->with($this->getPath())
+                     ->times(3)
+                     ->andReturn([
+                        $this->getTheme(),
+                        $this->getTheme('bar')
+                    ]);
+        $this->assertTrue($this->theme->has('foo'));
+        $this->assertTrue($this->theme->exists('bar'));
+        $this->assertFalse($this->theme->has('baz'));
+    }
+
+    public function testLoadViewFromCurrentTheme()
+    {
+        $this->config->shouldReceive('get')->once()->andreturn('foo');
+        $this->view->shouldReceive('make')->once()->andreturn('bar');
+        $response = $this->theme->view('index');
+        $this->assertEquals('bar', $response);
+    }
+
+    public function testLoadLangFromCurrentTheme()
+    {
+        $this->config->shouldReceive('get')->once()->andreturn('foo');
+        $this->lang->shouldReceive('get')->once()->andreturn('bar');
+        $result = $this->theme->lang('index');
+        $this->assertEquals('bar', $result);
+    }
+
+    public function testArrayableOfTheme()
+    {
+        $theme = new Theme([
+            'name' => 'foo',
+            'description' => 'foo theme',
+            'author' => [
+                'name' => 'Pingpong Labs',
+                'email' => 'pingpong.labs@gmail.com'
+            ],
+            'enabled' => true,
+        ]);
+
+        $array = $theme->toArray();
+
+        $this->assertArrayHasKey('name', $array);
+        $this->assertArrayHasKey('description', $array);
+        $this->assertArrayHasKey('author', $array);
+        $this->assertArrayHasKey('enabled', $array);
+    }
+
+    public function testGetAllThemesAsArray()
+    {
+        $this->finder->shouldReceive('find')
+                     ->with($this->getPath())
+                     ->once()
+                     ->andReturn([
+                        $this->getTheme(),
+                        $this->getTheme('bar')
+                    ]);
+
+        $themes = $this->theme->toArray();
 
         $this->assertTrue(is_array($themes));
         $this->assertArrayHasKey(0, $themes);
-        $this->assertArrayNotHasKey(1, $themes);
-    }
-
-    function testHasTheme()
-    {
-        $finder = m::mock('Pingpong\Themes\Finder');
-        $config = m::mock('Illuminate\Config\Repository');
-        $view = m::mock('Illuminate\View\Factory');
-        $lang = m::mock('Illuminate\Translation\Translator');
-
-        $theme = new Theme($finder, $config, $view, $lang);
-
-        $finder->shouldReceive('has')->once()->with('default')->andReturn(true);
-
-        $hasTheme = $theme->has('default');
-
-        $this->assertTrue($hasTheme);
-    }
-
-    function testGetCurrentThemeAndWillReturnFromConfig()
-    {
-        $finder = m::mock('Pingpong\Themes\Finder');
-        $config = m::mock('Illuminate\Config\Repository');
-        $view = m::mock('Illuminate\View\Factory');
-        $lang = m::mock('Illuminate\Translation\Translator');
-
-        $theme = new Theme($finder, $config, $view, $lang);
-
-        $config->shouldReceive('get')->once()->with('themes::default')->andReturn('default');
-
-        $currentTheme = $theme->getCurrent();
-
-        $this->assertEquals($currentTheme, 'default');
-    }
-
-    function testSetAndGetCurrentTheme()
-    {
-        $finder = m::mock('Pingpong\Themes\Finder');
-        $config = m::mock('Illuminate\Config\Repository');
-        $view = m::mock('Illuminate\View\Factory');
-        $lang = m::mock('Illuminate\Translation\Translator');
-
-        $theme = new Theme($finder, $config, $view, $lang);
-
-        $theme->setCurrent('white');
-
-        $currentTheme = $theme->getCurrent();
-
-        $this->assertEquals($currentTheme, 'white');
-    }
-
-    function testGetThemesPath()
-    {
-        $finder = m::mock('Pingpong\Themes\Finder');
-        $config = m::mock('Illuminate\Config\Repository');
-        $view = m::mock('Illuminate\View\Factory');
-        $lang = m::mock('Illuminate\Translation\Translator');
-
-        $theme = new Theme($finder, $config, $view, $lang);
-
-        $finder->shouldReceive('getPath')->once()->andReturn($this->getPath());
-
-        $path = $theme->getPath();
-
-        $this->assertEquals($path, $this->getPath());
-    }
-
-    function testThemePathForTheSpecifiedTheme()
-    {
-        $finder = m::mock('Pingpong\Themes\Finder');
-        $config = m::mock('Illuminate\Config\Repository');
-        $view = m::mock('Illuminate\View\Factory');
-        $lang = m::mock('Illuminate\Translation\Translator');
-
-        $theme = new Theme($finder, $config, $view, $lang);
-
-        $themePath = $this->getPath() . "/default";
-
-        $finder->shouldReceive('getThemePath')->once()->with('default')->andReturn($themePath);
-
-        $path = $theme->getThemePath('default');
-
-        $this->assertEquals($path, $themePath);
-    }
-
-    function testSetAndGetThemesPath()
-    {
-        $finder = m::mock('Pingpong\Themes\Finder');
-        $config = m::mock('Illuminate\Config\Repository');
-        $view = m::mock('Illuminate\View\Factory');
-        $lang = m::mock('Illuminate\Translation\Translator');
-
-        $theme = new Theme($finder, $config, $view, $lang);
-
-        $finder->shouldReceive('setPath')->once()->with($this->getPath())->andReturn($this->getPath());
-        $finder->shouldReceive('getPath')->once()->andReturn($this->getPath());
-
-        $theme->setPath($this->getPath());
-
-        $this->assertEquals($theme->getPath(), $this->getPath());
+        $this->assertArrayHasKey(1, $themes);
     }
 }
