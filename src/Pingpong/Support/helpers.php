a1f2e5fb3441caa2483bfc2e92127555e1123035
Gravitano <gravitano16@gmail.com>
2015-05-11 20:14:07 +0700

    Added gravatar helper to Support component

--- src/Pingpong/Support/helpers.php
+++ src/Pingpong/Support/helpers.php
@@ -85,3 +85,20 @@ if (! function_exists('error_for'))
         return $errors->first($field, $template);
     }
 }
+
+if (! function_exists('gravatar')) {
+    /**
+    * Gravatar URL from Email address
+    *
+    * @param string $email Email address
+    * @param string $size Size in pixels
+    * @param string $default Default image [ 404 | mm | identicon | monsterid | wavatar ]
+    * @param string $rating Max rating [ g | pg | r | x ]
+    *
+    * @return string
+    */
+    function gravatar($email, $size = 60, $default = 'mm', $rating = 'g')
+    {
+        return 'http://www.gravatar.com/avatar/' . md5(strtolower(trim($email))) . "?s={$size}&d={$default}&r={$rating}";
+    }
+}
