45b95066a8149e81a00c31695252e919edec7870
Gravitano <gravitano16@gmail.com>
2015-06-05 14:04:03 +0700

    CS Fixes

--- src/Pingpong/Presenters/Model.php
+++ src/Pingpong/Presenters/Model.php
@@ -1,9 +1,10 @@
-<?php namespace Pingpong\Presenters;
+<?php
+
+namespace Pingpong\Presenters;
 
 use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class Model extends Eloquent implements PresentableInterface
 {
-
     use PresentableTrait;
 }
