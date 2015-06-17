24393b26473909579d0ecd8d5caad4c634ab0ba9
Gravitano <gravitano16@gmail.com>
2015-06-17 22:35:20 +0700

    Squashed 'src/Pingpong/Support/' changes from 9401c19..a205752
    a205752 CS Fixes
    874240b Added new saveTo method, used for saving stub to specific path
    03eb788 added new createFromPath method: allow to create stub class from full path
    
    git-subtree-dir: src/Pingpong/Support
    git-subtree-split: a2057526e0b3bfadbb50edda879b20f7acb7496b

--- helpers.php
+++ helpers.php
@@ -3,42 +3,44 @@
 use Illuminate\Support\Facades\Request;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
-if (! function_exists('upload_image')) {
+if (!function_exists('upload_image')) {
     /**
      * Upload image and get image filename.
      *
-     * @param  UploadedFile $file
-     * @param  string $path
-     * @param  string $event
+     * @param UploadedFile $file
+     * @param string       $path
+     * @param string       $event
+     *
      * @return string
      */
     function upload_image($file, $path, $event = 'image.uploaded')
     {
-        if (! is_null($file)) {
-            $filename = sha1(time() . $file->getClientOriginalName()) . '.' . strtolower($file->getClientOriginalExtension());
+        if (!is_null($file)) {
+            $filename = sha1(time().$file->getClientOriginalName()).'.'.strtolower($file->getClientOriginalExtension());
 
             $file->move(public_path($path), $filename);
 
-            event($event, [$path . $filename]);
-            
+            event($event, [$path.$filename]);
+
             return $filename;
         }
 
-        return null;
+        return;
     }
 }
 
-if (! function_exists('set_active')) {
+if (!function_exists('set_active')) {
     /**
      * Set active to specified selector.
      *
      * @param array|string $paths
-     * @param string $class
+     * @param string       $class
+     *
      * @return string
      */
     function set_active($paths, $class = 'active')
     {
-        foreach ((array)$paths as $path) {
+        foreach ((array) $paths as $path) {
             if (Request::is($path)) {
                 return $class;
             }
@@ -46,13 +48,14 @@ if (! function_exists('set_active')) {
     }
 }
 
-if (! function_exists('flash')) {
+if (!function_exists('flash')) {
     /**
      * Flash message.
      *
      * @require "laracasts/flash:~1.3"
      *
-     * @param  string|null $message
+     * @param string|null $message
+     *
      * @return string|object
      */
     function flash($message = null)
@@ -67,12 +70,13 @@ if (! function_exists('flash')) {
     }
 }
 
-if (! function_exists('error_for')) {
+if (!function_exists('error_for')) {
     /**
      * Show validation for the specified field.
      *
-     * @param  string $field
-     * @param  object $errors
+     * @param string $field
+     * @param object $errors
+     *
      * @return string
      */
     function error_for($field, $errors, $template = '<div class="text-danger">:message</div>')
@@ -81,32 +85,33 @@ if (! function_exists('error_for')) {
     }
 }
 
-if (! function_exists('gravatar')) {
+if (!function_exists('gravatar')) {
     /**
-    * Gravatar URL from Email address
-    *
-    * @param string $email Email address
-    * @param string $size Size in pixels
-    * @param string $default Default image [ 404 | mm | identicon | monsterid | wavatar ]
-    * @param string $rating Max rating [ g | pg | r | x ]
-    *
-    * @return string
-    */
+     * Gravatar URL from Email address.
+     *
+     * @param string $email   Email address
+     * @param string $size    Size in pixels
+     * @param string $default Default image [ 404 | mm | identicon | monsterid | wavatar ]
+     * @param string $rating  Max rating [ g | pg | r | x ]
+     *
+     * @return string
+     */
     function gravatar($email, $size = 60, $default = 'mm', $rating = 'g')
     {
-        return 'http://www.gravatar.com/avatar/' . md5(strtolower(trim($email))) . "?s={$size}&d={$default}&r={$rating}";
+        return 'http://www.gravatar.com/avatar/'.md5(strtolower(trim($email)))."?s={$size}&d={$default}&r={$rating}";
     }
 }
 
-if (! function_exists('markdown')) {
+if (!function_exists('markdown')) {
     /**
      * Markdown.
      *
-     * @param  string $text
+     * @param string $text
+     *
      * @return string
      */
     function markdown($text)
     {
-        return (new ParsedownExtra)->text($text);
+        return (new ParsedownExtra())->text($text);
     }
 }
